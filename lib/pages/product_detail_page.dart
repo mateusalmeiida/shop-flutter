3962f9f971c7e2df03a16d5036c4d7e12d19233e
mateusalmeiida <mateusar222@gmail.com>
2025-02-28 11:59:47 -0300

    Implementada tela de detalhes dos produtos (ProductDetalPage)

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -10,6 +10,35 @@ class ProductDetailPage extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(title: Text(product.name)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'R\$${product.price}',
+              style: TextStyle(fontSize: 22, color: Colors.grey),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
