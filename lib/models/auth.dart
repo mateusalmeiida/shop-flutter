5af4f87c23f2d20e7bd3c53fd3e4c37c2b1fa6d4
mateusalmeiida <mateusar222@gmail.com>
2025-03-09 14:20:42 -0300

    Implementado login automatico

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
+import 'package:shop/data/store.dart';
 import 'package:shop/exceptions/auth_exceptions.dart';
 
 class Auth with ChangeNotifier {
@@ -55,10 +56,35 @@ class Auth with ChangeNotifier {
       _expiryDate =
           DateTime.now().add(Duration(seconds: int.parse(body['expiresIn'])));
 
+      Store.saveMap('userData', {
+        'token': _token,
+        'email': _email,
+        'userId': _userId,
+        'expiryDate': _expiryDate!.toIso8601String()
+      });
+
       _autoLogout();
 
       notifyListeners();
     }
+  }
+
+  Future<void> tryAutoLogin() async {
+    if (isAuth) return;
+
+    final userData = await Store.getMap('userData');
+    if (userData.isEmpty) return;
+
+    final expiryDate = DateTime.parse(userData['expiryDate']);
+    if (expiryDate.isBefore(DateTime.now())) return;
+
+    _token = userData['token'];
+    _email = userData['email'];
+    _userId = userData['userId'];
+    _expiryDate = expiryDate;
+
+    _autoLogout();
+    notifyListeners();
   }
 
   Future<void> signup(String email, String password) async {
@@ -76,7 +102,9 @@ class Auth with ChangeNotifier {
     _expiryDate = null;
     _clearLogoutTimer();
 
-    notifyListeners();
+    Store.remove('userData').then((_) {
+      notifyListeners();
+    });
   }
 
   void _clearLogoutTimer() {
